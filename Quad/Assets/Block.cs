520f403568af2a1780b5d74c191b49e296f4a24b
Mike Kentley <mike.kentley@gmail.com>
2024-05-28 15:37:38 -0700

    big commit with proper .gitignore

--- Quad/Assets/Block.cs
+++ Quad/Assets/Block.cs
@@ -2,31 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block
-{
+public class Block {
+
     public Mesh mesh;
+    Chunk parentChunk;
 
-    // Start is called before the first frame update
-    public Block(Vector3 offset, MeshUtils.BlockType type)
+    public Block(Vector3 offset, MeshUtils.BlockType type, Chunk chunk)
     {
-        Quad[] quads = new Quad[6];
-        quads[0] = new Quad(MeshUtils.BlockSide.BOTTOM, offset,type);
-        quads[1] = new Quad(MeshUtils.BlockSide.TOP, offset, type );
-        quads[2] = new Quad(MeshUtils.BlockSide.LEFT, offset, type );
-        quads[3] = new Quad(MeshUtils.BlockSide.RIGHT, offset, type );
-        quads[4] = new Quad(MeshUtils.BlockSide.FRONT, offset, type);
-        quads[5] = new Quad(MeshUtils.BlockSide.BACK, offset, type);
+        parentChunk = chunk;
+        Vector3 blockLocalPos = offset - chunk.location;
 
-        Mesh[] sideMeshes = new Mesh[6];
-        sideMeshes[0] = quads[0].mesh;
-        sideMeshes[1] = quads[1].mesh;
-        sideMeshes[2] = quads[2].mesh;
-        sideMeshes[3] = quads[3].mesh;
-        sideMeshes[4] = quads[4].mesh;
-        sideMeshes[5] = quads[5].mesh;
+        if (type != MeshUtils.BlockType.AIR)
+        {
+            List<Quad> quads = new List<Quad>();
+            if (!HasSolidNeighbour((int)blockLocalPos.x, (int)blockLocalPos.y - 1, (int)blockLocalPos.z))
+            {
+                if (type == MeshUtils.BlockType.GRASSSIDE)
+                    quads.Add(new Quad(MeshUtils.BlockSide.BOTTOM, offset, MeshUtils.BlockType.DIRT));
+                else
+                    quads.Add(new Quad(MeshUtils.BlockSide.BOTTOM, offset, type));
+            }
 
-       mesh = MeshUtils.MergeMeshes(sideMeshes);
+            if (!HasSolidNeighbour((int)blockLocalPos.x, (int)blockLocalPos.y + 1, (int)blockLocalPos.z))
+            {
+                if(type == MeshUtils.BlockType.GRASSSIDE) 
+                    quads.Add(new Quad(MeshUtils.BlockSide.TOP, offset, MeshUtils.BlockType.GRASSTOP));
+                else
+                    quads.Add(new Quad(MeshUtils.BlockSide.TOP, offset, type));
+            }
+
+
+            if (!HasSolidNeighbour((int)blockLocalPos.x - 1, (int)blockLocalPos.y, (int)blockLocalPos.z))
+                quads.Add(new Quad(MeshUtils.BlockSide.LEFT, offset, type));
+            if (!HasSolidNeighbour((int)blockLocalPos.x + 1, (int)blockLocalPos.y, (int)blockLocalPos.z))
+                quads.Add(new Quad(MeshUtils.BlockSide.RIGHT, offset, type));
+            if (!HasSolidNeighbour((int)blockLocalPos.x, (int)blockLocalPos.y, (int)blockLocalPos.z + 1))
+                quads.Add(new Quad(MeshUtils.BlockSide.FRONT, offset, type));
+            if (!HasSolidNeighbour((int)blockLocalPos.x, (int)blockLocalPos.y, (int)blockLocalPos.z - 1))
+                quads.Add(new Quad(MeshUtils.BlockSide.BACK, offset, type));
+
+            if (quads.Count == 0) return;
+
+            Mesh[] sideMeshes = new Mesh[quads.Count];
+            int m = 0;
+            foreach (Quad q in quads)
+            {
+                sideMeshes[m] = q.mesh;
+                m++;
+            }
+
+            mesh = MeshUtils.MergeMeshes(sideMeshes);
+            mesh.name = "Cube_0_0_0";
+        }
     }
 
-
+    public bool HasSolidNeighbour(int x, int y, int z)
+    {
+        if (x < 0 || x >= parentChunk.width ||
+            y < 0 || y >= parentChunk.height ||
+            z < 0 || z >= parentChunk.depth)
+        {
+            return false;
+        }
+        if(parentChunk.chunkData[x + parentChunk.width * (y + parentChunk.depth * z)] == MeshUtils.BlockType.AIR
+            || parentChunk.chunkData[x + parentChunk.width * (y + parentChunk.depth * z)] == MeshUtils.BlockType.WATER)
+        return false;
+        return true;
     }
+}
